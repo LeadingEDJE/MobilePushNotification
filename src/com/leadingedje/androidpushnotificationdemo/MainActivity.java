b3f49a23709f84c9be152f2d2a49b3f4292e920f
Keith Wedinger <keith.wedinger@leadingedje.com>
2014-04-17 07:03:31 -0400

    Remove unused fragment

--- src/com/leadingedje/androidpushnotificationdemo/MainActivity.java
+++ src/com/leadingedje/androidpushnotificationdemo/MainActivity.java
@@ -1,15 +1,11 @@
 package com.leadingedje.androidpushnotificationdemo;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -20,9 +16,6 @@ public class MainActivity extends Activity {
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
-//        if ( savedInstanceState == null ) {
-//            getFragmentManager().beginTransaction().add( R.id.container, new PlaceholderFragment() ).commit();
-//        }
         if ( PushNotificationRegistration.isGooglePlayServicesAvailable( this ) ) {
             // If Google Play Services are available, attempt to register
             // for push notifications
@@ -96,20 +89,4 @@ public class MainActivity extends Activity {
             tv.setText( extras.getString( Constants.CONTENT_INTENT_EXTRA_KEY ) );
         }
     }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
-            View rootView = inflater.inflate( R.layout.fragment_main, container, false );
-            return rootView;
-        }
-    }
-
 }
